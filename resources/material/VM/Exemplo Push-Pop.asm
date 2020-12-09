cf8e3129320f391977822dcec85c8a786e7c7a53
Thiago Miranda <marine.rlivielor@gmail.com>
2020-12-08 22:15:13 -0300

    alteração do push pop asm para ficar semelhante ao código do curso

--- resources/material/VM/Exemplo Push-Pop.asm
+++ resources/material/VM/Exemplo Push-Pop.asm
@@ -1,50 +1,10 @@
-push local 7 (LENTO)
+push local 7
 
 // addr = local + 7
-@7
-D=A
 @LCL
-A=M+D
 D=M
-// *SP = *addr
-@SP
-A=M
-M=D
-// SP++
-@SP
-M=M+1
-
-pop local 7 (LENTO)
-
-// addr = local + 7
 @7
-D=A
-@LCL
-D=D+M
-@R13
-M=D
-
-// SP--
-@SP
-M=M-1
-@SP
-A=M
-D=M
-
-// *addr = *SP
-@R13
-A=M
-M=D
-
-// ---------------------------------------------------------------------------------------------
-
-push local 7 (OTIMIZADO)
-
-// addr = local + 7
-@7
-D=A
-@LCL
-A=D+M
+A=D+A
 D=M
 // SP++ e *SP = *addr
 @SP
@@ -52,21 +12,19 @@ AM=M+1
 A=A-1
 M=D
 
-pop local 7 (OTIMIZADO)
+pop local 7
 
 // addr = local + 7
-@7
-D=A
 @LCL
-D=D+M
+D=M
+@7
+D=D+A
 @R13
 M=D
-
 // SP--
 @SP
 AM=M-1
 D=M
-
 // *addr = *SP
 @R13
 A=M
